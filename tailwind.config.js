a0f656faf0726d96d4ffadbf003436c503301efe
emih <emi.programming@gmail.com>
2022-08-31 16:06:44 +0900

    ➕ add tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-      content: ["./src/**/*.{html,js}"],
-      theme: {
-        extend: {},
-      },
-      plugins: [],
-    };
+  mode:"jit",
+  content:["src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
