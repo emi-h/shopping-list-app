39c505bffb553f8dc43ec11d73f6f2536a3cdc38
emih <emi.programming@gmail.com>
2022-10-11 14:30:29 +0900

    ♻️

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,22 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import { useState } from "react";
 import { mealData } from "src/componets/MealList/MealData";
+import { IngredienceType } from "src/type/IndredienceType";
 import { MealDataType } from "src/type/MealDataType";
+// import {
+//   ReduceValueArrayType,
+//   ReduceValueType,
+// } from "src/type/reduceValueArrayType";
 
 const Home: NextPage = () => {
   const [mealArray, setMealArray] = useState<MealDataType[]>(mealData);
   const [checkedMealArray, setCheckedMealArray] = useState<MealDataType[]>([]);
-  const [IngredienceArray, setIngredienceArray] = useState<any[]>([]);
+  const [IngredienceArray, setIngredienceArray] = useState<IngredienceType[]>(
+    []
+  );
+  // const [reduceNumber, setReduceNumber] = useState<number>();
 
+  // クリックしたメニューの配列を作成
   const handleMealchoise = (id: number) => {
     const newMealArray = mealArray.map((mealArray) => {
       if (mealArray.id === id) {
@@ -19,6 +28,7 @@ const Home: NextPage = () => {
     setMealArray(newMealArray);
   };
 
+  // 決定したメニューの材料の配列を作成
   const handleSetMeal = () => {
     const newMealArray = mealArray.filter((meal) => meal.checked);
     setCheckedMealArray(newMealArray);
@@ -28,7 +38,7 @@ const Home: NextPage = () => {
     setIngredienceArray(newIngredienceArray);
   };
 
-  // 同じ材料を計算して配列を作る
+  // 同じ材料を計算して配列を作成
   const total = IngredienceArray.reduce(
     (s1, e) =>
       Object.entries(e).reduce(
@@ -40,9 +50,17 @@ const Home: NextPage = () => {
       ),
     {}
   );
+  // console.log("total", total);
+
+  // 手持ちの材料をマイナスする
+  // const handleReduceIngredience = (e: ChangeEvent<HTMLInputElement>) => {
+  //   setReduceNumber(e.target.value);
+  //   console.log(e.target.value);
+  // };
+  // console.log("reduceValueArray", reduceValueArray);
 
   return (
-    <div className="m-8">
+    <div className="m-8 text-xl flex flex-col min-h-screen">
       <Head>
         <title>Shopping list generator</title>
         <meta
@@ -51,8 +69,8 @@ const Home: NextPage = () => {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1 className="text-center">Shopping list generator</h1>
-      <main>
+      <main className="flex-grow">
+        <h1 className="text-center text-5xl">Shopping list generator</h1>
         {/* Meal list */}
         <div className="pt-8 pb-8">
           <h2 className="font-bold text-2xl my-4">Meal list</h2>
@@ -93,34 +111,61 @@ const Home: NextPage = () => {
         <div className="pt-8 pb-8">
           <h2 className="font-bold text-2xl my-4">Ingredience list</h2>
           <div>
-            {Object.keys(total).map((e) => {
+            {Object.keys(total).map((i) => {
               return (
                 <>
-                  <div key={e}>
-                    <span>{e}</span>: <span>{total[e]}</span>
+                  <div key={i}>
+                    <span>{i}</span>: <span>{total[i]}</span>
                   </div>
                 </>
               );
             })}
           </div>
         </div>
+        {/* Ingredience you already have */}
+        {/* <div className="pt-8 pb-8">
+          <h2 className="font-bold text-2xl my-4">
+            Ingredience you already have
+          </h2>
+          <div>
+            {Object.keys(total).map((i) => {
+              return (
+                <>
+                  <div key={i} className="mb-4">
+                    <span>{i}</span>:{" "}
+                    <span>
+                      {" "}
+                      <input
+                        type="number"
+                        onChange={handleReduceIngredience}
+                        value={reduceNumber}
+                      />
+                    </span>
+                    <span> To shop → </span>
+                    <span>{total[i] - reduceNumber}</span>
+                  </div>
+                </>
+              );
+            })}
+          </div>
+        </div> */}
         {/* Shopping list */}
-        <div className="pt-8 pb-8">
+        {/* <div className="pt-8 pb-8">
           <h2 className="font-bold text-2xl my-4">Shopping list</h2>
           <ul>
-            {/* {mealIngredienceArray.map((mealIngre) => {
+            {mealIngredienceArray.map((mealIngre) => {
               return (
                 <li key={mealIngre}>
                   <div>mince:{mealIngre.mince}</div>
                 </li>
               );
-            })} */}
+            })}
           </ul>
-        </div>
+        </div> */}
       </main>
 
       <footer>
-        <p className="m-100 text-center">Made by Emi</p>
+        <p className="m-100 text-center">Made by Emi in 2022</p>
       </footer>
     </div>
   );
---
+++ src/type/IndredienceType.ts
@@ -0,0 +1,3 @@
+export type IngredienceType = {
+  [prop: string]: number;
+};
