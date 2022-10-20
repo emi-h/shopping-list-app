1c42b10c6d33a821aa6bacfbff2b64e522fec046
emih <emi.programming@gmail.com>
2022-10-20 17:55:55 +0900

    🔥

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,6 @@ import "../styles/globals.css";
 import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { Session, SessionContextProvider } from "@supabase/auth-helpers-react";
 import type { AppProps } from "next/app";
-// import { useRouter } from "next/router";
 import { useState } from "react";
 
 function MyApp({
