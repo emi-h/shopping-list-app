2fe543eb0bc601af6a8dda0698ef5098ea7ede23
emih <emi.programming@gmail.com>
2022-10-21 11:05:28 +0900

    ♻️ add a Nav component

--- src/pages/account.tsx
+++ src/pages/account.tsx
@@ -2,19 +2,14 @@ import { useSession } from "@supabase/auth-helpers-react";
 import { NextPage } from "next";
 import Link from "next/link";
 import { Account } from "src/componets/Account";
+import { Nav } from "src/componets/Nav";
 
 const AccountPage: NextPage = () => {
   const session = useSession();
 
   return (
     <>
-      <nav>
-        <li>
-          <Link href="/">
-            <a>メニュー選択</a>
-          </Link>
-        </li>
-      </nav>
+      <Nav />
       <div className="container" style={{ padding: "50px 0 100px 0" }}>
         {!session ? null : <Account session={session} />}
       </div>
