4e94b94d4379f6c4e8d6d3e47767a96b35887994
emih <emi.programming@gmail.com>
2022-10-22 09:40:15 +0900

    ⚡️ remove tailwind css and Mantine conflict

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode:"jit",
-  content:["src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
+  content: ["src/**/*.{js,ts,jsx,tsx}"],
+  mode: "jit",
   plugins: [],
+  theme: {
+    /* remove tailwind css and Mantine conflict */
+    screens: {
+      lg: "1200px",
+      md: "992px",
+      sm: "768px",
+      xl: "1400px",
+      xs: "576px",
+    },
+  },
 };
