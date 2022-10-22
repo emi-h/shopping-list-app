26e326fdd2e3024078c0c3d953d9b40b52d367ee
emih <emi.programming@gmail.com>
2022-10-22 11:45:42 +0900

    💄 add styles

--- src/componets/App.tsx
+++ src/componets/App.tsx
@@ -78,89 +78,90 @@ export const App: FC = () => {
   // console.log("reduceValueArray", reduceValueArray);
 
   return (
-    <div className="m-8 text-xl flex flex-col min-h-screen">
-      <Head>
-        <title>Shopping list generator</title>
-        <meta
-          name="description"
-          content="Generate your shopping list by choosing recipes!"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div className="container mx-auto px-4">
+      <div className="flex flex-col min-h-screen">
+        <Head>
+          <title>Shopping list generator</title>
+          <meta
+            name="description"
+            content="Generate your shopping list by choosing recipes!"
+          />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <main className="flex-grow">
-        <h1 className="text-center text-3xl">
-          --- Shopping list generator ---
-        </h1>
-        <Nav />
-        {/* Meal list */}
-        <div className="pt-8 pb-8">
-          <h2 className="font-bold text-2xl my-4">
-            1.Meal list：Choose menu and click the button.
-          </h2>
-          <div>
-            {mealArray.map((meal) => {
-              return (
-                <div key={meal.id}>
-                  <button
-                    onClick={() => {
-                      handleMealchoise(meal.id);
-                    }}
-                    className="mr-4"
-                  >
-                    <span className="mr-2">{meal.checked ? "○" : "✖︎"}</span>
-                    <span>{meal.name}</span>
-                  </button>
-                </div>
-              );
-            })}
+        <main className="flex-grow">
+          <div className="flex align-center justify-between">
+            <h1 className="text-center text-xl">Shopping list generator :)</h1>
+            <Nav />
           </div>
-          <button className="border p-4 mt-8" onClick={handleSetMeal}>
-            Set a meal list
-          </button>
-        </div>
-        {/* Meal list(ticked) */}
-        <div className="pt-8 pb-8">
-          <h2 className="font-bold text-2xl my-4">
-            2.Meal list(ticked)：check the menu you chose.
-          </h2>
-          <div>
-            {checkedMealArray.map((meal) => {
-              return (
-                <div key={meal.id}>
-                  <div>{meal.name}</div>
-                </div>
-              );
-            })}
-          </div>
-        </div>
-        {/* <IngredienceList total={total} /> */}
-        <div className="pt-8 pb-8">
-          <h2 className="font-bold text-2xl my-4">
-            3.Ingredience list：Generated the ingredience list from your meal.
-          </h2>
-          <div className="mb-4">
-            {/* Object.keys(total)でkeyの配列を作成 */}
-            {Object.keys(total).map((i) => {
-              return (
-                <>
-                  <div key={i}>
-                    <span>{i}</span>: <span>{total[i]}</span>
+          {/* Meal list */}
+          <div className="pt-8 pb-8">
+            <h2 className="font-bold text-2xl my-4">
+              1.Meal list：Choose menu and click the button.
+            </h2>
+            <div>
+              {mealArray.map((meal) => {
+                return (
+                  <div key={meal.id}>
+                    <button
+                      onClick={() => {
+                        handleMealchoise(meal.id);
+                      }}
+                      className="mr-4"
+                    >
+                      <span className="mr-2">{meal.checked ? "○" : "✖︎"}</span>
+                      <span>{meal.name}</span>
+                    </button>
                   </div>
-                </>
-              );
-            })}
+                );
+              })}
+            </div>
+            <button className="border p-4 mt-8" onClick={handleSetMeal}>
+              Set a meal list
+            </button>
           </div>
-          <CopyButton value={copyText}>
-            {({ copied, copy }) => (
-              <Button color={copied ? "teal" : "blue"} onClick={copy}>
-                {copied ? "Copied!" : "Copy Ingredience list"}
-              </Button>
-            )}
-          </CopyButton>
-        </div>
-        {/* Ingredience you already have */}
-        {/* <div className="pt-8 pb-8">
+          {/* Meal list(ticked) */}
+          <div className="pt-8 pb-8">
+            <h2 className="font-bold text-2xl my-4">
+              2.Meal list(ticked)：check the menu you chose.
+            </h2>
+            <div>
+              {checkedMealArray.map((meal) => {
+                return (
+                  <div key={meal.id}>
+                    <div>{meal.name}</div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+          {/* <IngredienceList total={total} /> */}
+          <div className="pt-8 pb-8">
+            <h2 className="font-bold text-2xl my-4">
+              3.Ingredience list：Generated the ingredience list from your meal.
+            </h2>
+            <div className="mb-4">
+              {/* Object.keys(total)でkeyの配列を作成 */}
+              {Object.keys(total).map((i) => {
+                return (
+                  <>
+                    <div key={i}>
+                      <span>{i}</span>: <span>{total[i]}</span>
+                    </div>
+                  </>
+                );
+              })}
+            </div>
+            <CopyButton value={copyText}>
+              {({ copied, copy }) => (
+                <Button color={copied ? "teal" : "blue"} onClick={copy}>
+                  {copied ? "Copied!" : "Copy Ingredience list"}
+                </Button>
+              )}
+            </CopyButton>
+          </div>
+          {/* Ingredience you already have */}
+          {/* <div className="pt-8 pb-8">
               <h2 className="font-bold text-2xl my-4">
                 Ingredience you already have
               </h2>
@@ -186,8 +187,8 @@ export const App: FC = () => {
                 })}
               </div>
             </div> */}
-        {/* Shopping list */}
-        {/* <div className="pt-8 pb-8">
+          {/* Shopping list */}
+          {/* <div className="pt-8 pb-8">
               <h2 className="font-bold text-2xl my-4">Shopping list</h2>
               <ul>
                 {mealIngredienceArray.map((mealIngre) => {
@@ -199,9 +200,10 @@ export const App: FC = () => {
                 })}
               </ul>
             </div> */}
-      </main>
+        </main>
 
-      <Footer />
+        <Footer />
+      </div>
     </div>
   );
 };
--- src/componets/Nav.tsx
+++ src/componets/Nav.tsx
@@ -4,20 +4,20 @@ import { FC } from "react";
 export const Nav: FC = () => {
   return (
     <nav>
-      <ul className="flex items-center justify-center">
+      <ul className="flex items-center justify-center align-center h-full">
         <li>
           <Link href="/">
-            <a className="border p-4 mt-8 inline-block">メニュー選択</a>
+            <a className="border pl-4 inline-block">メニュー選択</a>
           </Link>
         </li>
         <li>
           <Link href="add">
-            <a className="border p-4 mt-8 inline-block">メニュー追加</a>
+            <a className="border pl-4 inline-block">メニュー追加</a>
           </Link>
         </li>
         <li>
           <Link href="account">
-            <a className="border p-4 mt-8 inline-block">アカウント情報変更</a>
+            <a className="border pl-4 inline-block">アカウント情報変更</a>
           </Link>
         </li>
       </ul>
